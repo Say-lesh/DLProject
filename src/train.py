6e1bd8d84b77ea8271134e1be45661ac8df58a98
Say-lesh <shaileshp.work@gmail.com>
2023-06-26 16:14:38 +0530

    Adding Project files

---
+++ src/train.py
@@ -0,0 +1,29 @@
+from monai.networks.nets import UNet
+from monai.networks.layers import Norm
+from monai.losses import DiceLoss, DiceCELoss
+
+import torch
+from preporcess import prepare
+from modeling import train
+
+data_dir = '../data'
+model_dir = '../models'
+
+data_in = prepare(data_dir, cache=False)
+
+device = torch.device("cuda:0")
+model = UNet(
+    dimensions=3,
+    in_channels=1,
+    out_channels=2,
+    channels=(16, 32, 64, 128, 256), 
+    strides=(2, 2, 2, 2),
+    num_res_units=2,
+    norm=Norm.BATCH,
+).to(device)
+
+loss_function = DiceLoss(to_onehot_y=True, sigmoid=True, squared_pred=True)
+optimizer = torch.optim.Adam(model.parameters(), 1e-5, weight_decay=1e-5, amsgrad=True)
+
+if __name__ == '__main__':
+    train(model, data_in, loss_function, optimizer, 100, model_dir)
